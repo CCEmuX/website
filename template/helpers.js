f8aa83b7abe5126bc6ad4447549bb52910a827b9
SquidDev <bonzoweb@hotmail.co.uk>
2018-07-22 09:29:18 +0000

    Some additional improvements to the documentation page
     - Add a navigation sidebar with links to each header. Similarly, add
       permalink links to each header.
     - Add some images for a couple of the sections.

--- template/helpers.js
+++ template/helpers.js
@@ -3,4 +3,15 @@ module.exports.register = handlebars => handlebars.registerHelper({
     const extra = linkTo == context.url ? ` class="active"` : '';
     return `<a href="${linkTo}" ${extra}>${options.fn(this)}</a>`;
   },
+
+  header: function(name, context, options) {
+    const body = options.fn(this);
+    (context.headers || (context.headers = [])).push({ name, body });
+    return `<a class="header-link" href="#${name}" name=${name}>^</a>${body}`;
+  },
+
+  headers: function(context) {
+    const items = (context.headers || []).map(({ name, body }) => `<li><a href="#${name}">${body}</a>`).join("");
+    return new handlebars.SafeString(items);
+  }
 });
